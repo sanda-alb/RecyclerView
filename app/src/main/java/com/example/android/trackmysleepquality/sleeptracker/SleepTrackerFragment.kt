fe8175747fbc4463d0bf295add58df01f5adf5f1
albrecht <sanda.albrecht@gmail.com>
2021-08-12 13:11:13 +0300

    ViewHolder for all the sleep data

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -73,6 +73,16 @@ class SleepTrackerFragment : Fragment() {
         // This is necessary so that the binding can observe LiveData updates.
         binding.lifecycleOwner = this
 
+
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter= adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         // Add an Observer on the state variable for showing a Snackbar message
         // when the CLEAR button is pressed.
         sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner, Observer {
@@ -107,5 +117,9 @@ class SleepTrackerFragment : Fragment() {
             }
         })
         return binding.root
+
+
     }
+
+
 }
