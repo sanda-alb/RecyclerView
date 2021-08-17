9c6598321f6ecbc6b60bbcebc6263ebd6cdf2c59
albrecht <sanda.albrecht@gmail.com>
2021-08-17 12:00:32 +0300

    made items clickable

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -75,7 +76,9 @@ class SleepTrackerFragment : Fragment() {
         binding.lifecycleOwner = this
 
 
-        val adapter = SleepNightAdapter()
+        val adapter = SleepNightAdapter(SleepNightListener { nightId ->
+            Toast.makeText(context, "${nightId}", Toast.LENGTH_LONG).show()
+        })
         binding.sleepList.adapter= adapter
 
         // ListAdapter provides a method called submitList() to tell ListAdapter
